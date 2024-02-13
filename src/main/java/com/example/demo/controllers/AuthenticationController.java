30d07e314d5e0a4cda4ed98bf4841ea84df2c1d2
Gourav Kumar <91111185+gourav2982@users.noreply.github.com>
2024-02-13 19:36:48 +0530

    Auth Controller updated

--- src/main/java/com/example/demo/controllers/AuthenticationController.java
+++ src/main/java/com/example/demo/controllers/AuthenticationController.java
@@ -6,10 +6,7 @@ import com.example.demo.dto.SignInRequest;
 import com.example.demo.dto.SignUpRequest;
 import com.example.demo.services.AuthenticationService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1")
@@ -17,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class AuthenticationController {
 
     private final AuthenticationService authenticationService;
+
 
     @PostMapping("/signup")
     public JwtAuthenticationResponse signup(@RequestBody SignUpRequest request) {
